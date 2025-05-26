8600b92542ddafad5890c192cee6406d3f71e43b
Aiduoxila <159774819+Aiduoxila@users.noreply.github.com>
2025-05-26 07:56:34 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,8 +2,8 @@
     'name': 'Czech Payment Integration',
     'version': '1.0',
     'category': 'Accounting',
-    'summary': 'Integrate local Czech payment providers like GoPay and PayU Czech into Odoo',
-    'author': 'Your Name',
+    'summary': 'Integrate local Czech payment providers like pays, GoPay and PayU Czech into Odoo',
+    'author': 'aiduoxila.com',
     'depends': ['account', 'payment'],
     'data': [
         'views/payment_provider_views.xml',
